49ccbd8571a80ddecee0d38520f5af59961ec143
MD Waliullah <waliullah9099@gmail.com>
2025-04-26 23:41:14 +0600

    first commit

---
+++ scope.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+var a = 20
+var b = 30
+
+
+
+func add(x int, y int){
+ 	var z = x + y
+
+	fmt.Println(z)
+}
+
+func main(){
+	var p = 30
+	var q = 40
+
+
+	add(p, q)
+
+	add(a, b)
+
+	add(a, p)
+
+}
+
+
