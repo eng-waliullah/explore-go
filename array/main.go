4fe45cccc05ebe0b00a228ec2429eba993620f74
MD Waliullah <waliullah9099@gmail.com>
2025-04-30 08:34:25 +0600

    array

---
+++ array/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+var arr3 = [3]string { "Ami", "Tanvirer", "Mama" }
+
+
+func main(){
+
+	var arr [2]int
+
+	arr2 := [2]int {2, 7}
+
+	arr[1] = 6
+
+
+	arr3[0] = "Hello"
+	arr3[2] = "Mami"
+
+	fmt.Println(arr)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+ 
+}
+
+
