af2d642cb4e5d5baf6e4e3f3ed0fc609e4282767
MD Waliullah <waliullah9099@gmail.com>
2025-05-15 10:13:11 +0600

    extend the knowledge about array in go

--- array/main.go
+++ array/main.go
@@ -2,25 +2,53 @@ package main
 
 import "fmt"
 
-var arr3 = [3]string { "Ami", "Tanvirer", "Mama" }
+var arr3 = [3]string{"Ami", "Tanvirer", "Mama"}
 
+func main() {
 
-func main(){
+	// var arr [2]int
 
-	var arr [2]int
+	// arr2 := [2]int {2, 7}
 
-	arr2 := [2]int {2, 7}
+	// arr[1] = 6
 
-	arr[1] = 6
+	// arr3[0] = "Hello"
+	// arr3[2] = "Mami"
 
+	// fmt.Println(arr)
+	// fmt.Println(arr2)
+	// fmt.Println(arr3)
 
-	arr3[0] = "Hello"
-	arr3[2] = "Mami"
+	// numbers := [8]int{1, 2, 3, 4, 5, 6, 7, 8}
 
-	fmt.Println(arr)
-	fmt.Println(arr2)
+	// for i := 0; i < len(numbers); i++ {
+	// 	fmt.Println(numbers[i])
+	// }
+
+	// for index, value := range numbers {
+	// 	fmt.Println(index, value)
+	// }
+
+	// var multi [3][3]int
+
+	// multi := [3][3]int{
+	// 	{1, 2, 3},
+	// 	{1, 2, 3},
+	// 	{1, 2, 3},
+	// }
+	// fmt.Println(multi)
+
+	// arr1 := [5]int{1, 2, 3, 4, 5}
+	// arr2 := [5]int{1, 2, 3, 4, 5}
+
+	// fmt.Println(arr1)
+	// fmt.Println(arr2)
+
+	var arr3 [5]int
+
+	for i := 0; i < len(arr3); i++ {
+		fmt.Scan(&arr3[i])
+	}
 	fmt.Println(arr3)
- 
+
 }
-
-
