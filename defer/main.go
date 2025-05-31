6b72956ea43f0f99b08f4accb30f8a816783f515
MD Waliullah <waliullah9099@gmail.com>
2025-05-31 20:58:54 +0600

    explore about defer func

--- defer/main.go
+++ defer/main.go
@@ -1,3 +1,26 @@
+// defer fucntion ---> under the hood closer
+
+// parent & closer jodi exists kore tahole closer variable ar address refer share kore
+
+/*
+
+named returned values rules
+
+1. all code executed
+2. defer function store kora hobe magic box e
+3. return --> all defer fucntion execute korbe
+4. return korbe named variables gular values
+
+
+just return types
+1. all code executed
+2. defer function store kora hobe magic box e
+3. return values are evaluated at this time (store the return value)
+4. all defer functions execute korbe
+
+
+*/
+
 package main
 
 import "fmt"
@@ -64,9 +87,9 @@ func calc() int {
 func main() {
 
 	a := calculate()
-	b := calc()
+	fmt.Println("main first", a)
 
-	fmt.Println(a)
-	fmt.Println(b)
+	b := calc()
+	fmt.Println("main second", b)
 
 }
