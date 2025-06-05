2720c19b87fedcbdbcdf1a141fad727564a6a347
MD Waliullah <waliullah9099@gmail.com>
2025-06-05 23:52:21 +0600

    Go Routine

--- Keywords/declarationKeywords.go
+++ Keywords/declarationKeywords.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+// func main() {
+// 	a := 333
+
+// 	fmt.Println(a)
+// }
+
+import (
+	"fmt"
+	"runtime"
+)
 
 func main() {
-	a := 333
-
-	fmt.Println(a)
+	fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(0)) // Number of Ps
+	for i := 0; i < 10; i++ {
+		go func(id int) {
+			for {
+				fmt.Printf("Goroutine %d running\n", id)
+			}
+		}(i)
+	}
+	select {} // Keep main goroutine alive
 }
