011ea49782174adc2c681c414536630c51bc567b
MD Waliullah <waliullah9099@gmail.com>
2025-06-20 19:41:37 +0600

    Compplex Routine

--- complex-routine/main.go
+++ complex-routine/main.go
@@ -1,5 +1,31 @@
 package main
 
+/*
+
+------ main go routine, stack create korbe heap e.
+
+
+----------- Go Routine ~~~ only 2 kb (thread hote hole stack lagbe ar stack 8mb)
+----------- or mini thread
+----------- or virtual thread
+----------- or logical thread
+
+
+
+
+1. Go Routine Scheduler
+2. Heap Allocator (K initialize kore )
+3. Garbage Collertor
+4. Logical Processors Create kore
+5.
+
+
+## code execute kore akmatro CPU
+
+
+
+*/
+
 import (
 	"fmt"
 	"time"
@@ -10,6 +36,7 @@ var a = 10
 const p = 11
 
 func printHello(num int) {
+	time.Sleep(5 * time.Second)
 	fmt.Println("Hello Habiba", "--", num)
 }
 
@@ -24,17 +51,15 @@ func main() {
 	println(a, " ", p)
 
 	time.Sleep(5 * time.Second)
-
+	fmt.Println("Hello Habiba")
 }
 
 /*
 
 
-build main file will save in HDD
-
-threed stack re execute kore
-
-OS kurnel aise stack re execute kore
+a. build main file will save in HDD
+b. threed stack re execute kore
+c. OS kurnel aise stack re execute kore
 
 
 
