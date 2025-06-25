81ed248a40f491f1ed2f3595193562ad81f37eb3
MD Waliullah <waliullah9099@gmail.com>
2025-06-25 20:50:13 +0600

    variadic function

--- slice/main.go
+++ slice/main.go
@@ -14,73 +14,100 @@ func print(nums ...int) { // variadic function as like spreed operator in JS
 	fmt.Println(cap(nums))
 }
 
+func variadic(numbers ...int) {
+	fmt.Println(numbers)
+}
+
 func main() {
 
 	// pointer // address // memory ( ram / hard disk )
 
 	/*
-		arr := [5]string{"This", "is", "GO", "programmig", "language"}
+						arr := [5]string{"This", "is", "GO", "programmig", "language"}
 
-		fmt.Println(arr)
+						fmt.Println(arr)
 
-		s := arr[1:4]
-		fmt.Println(s)
-		fmt.Println(len(s))
+						s := arr[1:4]
+						fmt.Println(s)
+						fmt.Println(len(s))
+						fmt.Println(cap(s))
 
-		s1 := s[1:2]
-		fmt.Println(s1)
-		fmt.Println(len(s1))
-		fmt.Println(cap(s1))
 
-		s2 := []int{1, 2, 3} // slice literal
-		fmt.Println("slice: ", s2, "len: ", len(s2), "cap: ", cap(s2))
 
-		s3 := make([]int, 3)
-		s3[0] = 5
-		fmt.Println("slice: ", s3, "len: ", len(s3), "cap: ", cap(s3))
 
-		s4 := make([]int, 3, 5)
-		s4[0] = 5
-		// s4[3] = 7
-		fmt.Println("slice: ", s4, "len: ", len(s4), "cap: ", cap(s4))
+						s1 := s[1:2]
+						fmt.Println(s1)
+						fmt.Println(len(s1))
+						fmt.Println(cap(s1))
+						s2 := []int{1, 2, 3} // slice literal
+						fmt.Println("slice: ", s2, "len: ", len(s2), "cap: ", cap(s2))
 
-		var g []int //  empty or nil slice
-		g = append(g, 1)
-		fmt.Println("slice: ", g, "len: ", len(g), "cap: ", cap(g))
+						// =============
+
+					s3 := make([]int, 3)
+					s3[0] = 5
+					fmt.Println("slice: ", s3, "len: ", len(s3), "cap: ", cap(s3))
+
+					s4 := make([]int, 3, 5)
+					s4[0] = 5
+					s4[2] = 7
+					fmt.Println("slice: ", s4, "len: ", len(s4), "cap: ", cap(s4))
+
+					var g []int //  empty or nil slice
+					g = append(g, 1)
+					fmt.Println("slice: ", g, "len: ", len(g), "cap: ", cap(g))
+
+
+			// ====================
+
+				s := make([]int, 1024) // len: 1024, cap: 1024
+				fmt.Println(len(s), cap(s))
+
+				s = append(s, 1025) // len: 1025, cap: 1280 (1024 + 25% of 1024)
+				fmt.Println(len(s), cap(s))
+
+				s = append(s, 1300) // len: 1300, cap: 1600 (1280 + 25% of 1280)
+				fmt.Println(len(s), cap(s))
+
+					// ============
+
+		variadic(1, 2, 3, 4, 5, 6)
+
+					// ==============================
+
+					var x []int      // [], len = 0, cap = 0
+					x = append(x, 1) // [1], len = 1, cap = 1
+					x = append(x, 2) // [1, 2], len = 2, cap = 2
+					x = append(x, 3) // [1, 2, 3], len = 3, cap = 3
+
+					y := x
+
+					x = append(x, 4) // [1, 2, 3, 4], len = 4, cap = 4
+					y = append(y, 5) // [], len = 0, cap = 0
+
+					x[0] = 10
+
+					fmt.Println("slice: ", x, "len: ", len(x), "cap: ", cap(x)) // [10 2 3 5]
+					fmt.Println("slice: ", y, "len: ", len(y), "cap: ", cap(y)) // [10 2 3 5]
+
+					p := []int{1, 2, 3, 4, 5}
+
+					p = append(p, 6)
+					p = append(p, 7)
+
+					a := p[4:]
+
+					q := changeSlice(a)
+
+					fmt.Println(p) //  [1 2 3 4 10 6 7]
+					fmt.Println(q) //  [10 6 7 11]
+
+					fmt.Println(p[0:8]) //  [1 2 3 4 10 6 7 11]
+
+
+
 
 	*/
-
-	/*
-
-		var x []int      // [], len = 0, cap = 0
-		x = append(x, 1) // [1], len = 1, cap = 1
-		x = append(x, 2) // [1, 2], len = 2, cap = 2
-		x = append(x, 3) // [1, 2, 3], len = 3, cap = 3
-
-		y := x
-
-		x = append(x, 4) // [1, 2, 3, 4], len = 4, cap = 4
-		y = append(y, 5) // [], len = 0, cap = 0
-
-		x[0] = 10
-
-		fmt.Println("slice: ", x, "len: ", len(x), "cap: ", cap(x)) // [10 2 3 5]
-		fmt.Println("slice: ", y, "len: ", len(y), "cap: ", cap(y)) // [10 2 3 5]
-	*/
-
-	p := []int{1, 2, 3, 4, 5}
-
-	p = append(p, 6)
-	p = append(p, 7)
-
-	a := p[4:]
-
-	q := changeSlice(a)
-
-	fmt.Println(p) //  [1 2 3 4 10 6 7]
-	fmt.Println(q) //  [10 6 7 11]
-
-	fmt.Println(p[0:8]) //  [1 2 3 4 10 6 7 11]
 
 	// ab := []int{1, 2, 3, 3}
 	// ab := make([]int, 6, 10)
